95a21472d962cfe2ac4d8082cdce7a901e8a67b2
Deepa Vasudevan <deepa.vasudevan@activenetwork.com>
2014-01-06 13:52:46 -0800

    Added a simple function

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -13,7 +13,17 @@ namespace TestGit
             //This is a test for git repo.
             Console.Write("Create an empty repository");
             Console.Write("Hello! Have a great day!");
+            
+            //Call the function GetQuote
+            Console.Write("Calling function GetQuote " + GetQuote(2) );
             Console.ReadLine();
+        }
+
+        static string GetQuote(int i)
+        {
+            //After clculation return quote
+            int result = i * i;
+            return (result.ToString());
         }
     }
 }

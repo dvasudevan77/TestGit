1603ea79de15f5f1c8ded66078a8e434c5e49b32
Deepa Vasudevan <deepa.vasudevan@activenetwork.com>
2014-01-09 12:57:34 -0800

    Added another message

--- TestGit/Utility.cs
+++ TestGit/Utility.cs
@@ -8,12 +8,16 @@ namespace TestGit
 {
     public static class Utility
     {
+<<<<<<< HEAD
         /// <summary>
         /// Method for sum of two numbers. Bal bla
         /// </summary>
         /// <param name="number1"></param>
         /// <param name="number2"></param>
         /// <returns></returns>
+=======
+        
+>>>>>>> parent of 2493117... Added files
         public static int sum(int number1, int number2)
         {
             int test = number1 + number2;

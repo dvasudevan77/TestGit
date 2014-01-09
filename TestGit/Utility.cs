332c380d3380980c3668f1710af724067b46ae66
Deepa Vasudevan <deepa.vasudevan@activenetwork.com>
2014-01-09 13:09:02 -0800

    Remove "Removed bla bla"
    This reverts commit f8ed2aebe85076a14f27dc1ef31f0286e985c0fc.
    
    "Remove bla bla"

--- TestGit/Utility.cs
+++ TestGit/Utility.cs
@@ -9,7 +9,7 @@ namespace TestGit
     public static class Utility
     {
         /// <summary>
-        /// Method for sum of two numbers. 
+        /// Method for sum of two numbers. Bal bla
         /// </summary>
         /// <param name="number1"></param>
         /// <param name="number2"></param>

249311741a0466cb9c5800d0e8f06905fae60c0d
Deepa Vasudevan <deepa.vasudevan@activenetwork.com>
2014-01-09 12:25:38 -0800

    Added files

--- TestGit/Utility.cs
+++ TestGit/Utility.cs
@@ -8,7 +8,12 @@ namespace TestGit
 {
     public static class Utility
     {
-        
+        /// <summary>
+        /// Method for sum of two numbers.
+        /// </summary>
+        /// <param name="number1"></param>
+        /// <param name="number2"></param>
+        /// <returns></returns>
         public static int sum(int number1, int number2)
         {
             int test = number1 + number2;
